c4f71a9976a63fc22819d4b173511d773fec4295
Dmitry Fal <fal.dmitry@gmail.com>
2016-01-04 13:57:45 +0200

    Fix MSSQL setup path

--- scripts/install-sql-server.cmd
+++ scripts/install-sql-server.cmd
@@ -1,6 +1,6 @@
 @echo off
 echo Installing SQL Server 2008 Express R2, it will take a while...
 C:\vagrant\downloads\SQLEXPRWT_x64_ENU.exe /Q
-C:\Windows\system32\SQLEXPRWT_x64_ENU\SETUP.EXE /Q /Action=install /INSTANCENAME="SQLEXPRESS" /INSTANCEID="SQLExpress" /IAcceptSQLServerLicenseTerms /FEATURES=SQL,Tools /TCPENABLED=1 /SECURITYMODE="SQL" /SAPWD="#SAPassword!"
+C:\Windows\SysWOW64\SQLEXPRWT_x64_ENU\SETUP.EXE /Q /Action=install /INSTANCENAME="SQLEXPRESS" /INSTANCEID="SQLExpress" /IAcceptSQLServerLicenseTerms /FEATURES=SQL,Tools /TCPENABLED=1 /SECURITYMODE="SQL" /SAPWD="#SAPassword!"
 echo DONE!
 
