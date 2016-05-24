8cf5a9384c24b9971fb2fbd597616abe0ef2d2cb
Dmitry Fal <fal.dmitry@gmail.com>
2016-05-24 19:57:58 +0300

    Fix logins for attached DBs

--- scripts/attach-db.ps1
+++ scripts/attach-db.ps1
@@ -3,16 +3,45 @@ Import-Module SQLPS
 $configFile = "C:\vagrant\DB\config.json"
 $config = (Get-Content $configFile -Raw) | ConvertFrom-Json
 
+If ($config.logins -ne $null) {
+	ForEach ($login in $config.logins) {
+		$name = $login.name
+		$password = $login.password
+
+		$loginSql = "
+			USE [master]
+			CREATE LOGIN [$name] WITH PASSWORD=N'$password', DEFAULT_DATABASE=[master], CHECK_EXPIRATION=OFF, CHECK_POLICY=OFF
+		"
+		Invoke-Sqlcmd $loginSql -QueryTimeout 3600 -ServerInstance '.\SQLExpress'
+	}
+}
+
 ForEach ($db in $config.databases) {
 	$name = $db.name
 	$mdf = $db.mdf
 	$ldf = $db.ldf
+	$users = $db.users
 
 	$attachSql = "
 		USE [master]
-		GO
 		CREATE DATABASE [$name] ON (FILENAME = '$mdf.mdf'),(FILENAME = '$ldf.ldf') for ATTACH
-		GO
 	"
 	Invoke-Sqlcmd $attachSql -QueryTimeout 3600 -ServerInstance '.\SQLExpress'
+
+	If ($users -ne $null) {
+		ForEach($user in $users) {
+			$userName = $user.userName
+			$loginName = $user.loginName
+
+			$userSql = "
+				USE [$name]
+				IF EXISTS (SELECT * FROM sys.database_principals WHERE name = N'$userName')
+					EXEC sp_change_users_login 'Auto_Fix', '$userName'
+				ELSE
+					CREATE USER [$userName] FOR LOGIN [$loginName]
+				EXEC sp_addrolemember N'db_owner', N'$userName'
+			"
+			Invoke-Sqlcmd $userSql -QueryTimeout 3600 -ServerInstance '.\SQLExpress'
+		}
+	}
 }
